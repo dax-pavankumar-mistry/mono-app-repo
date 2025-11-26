56a25963ad39e4cc756ccfda648a5a4308eafe16
dax-pavankumar-mistry <pavankumar.mistry@daxwell.com>
2025-11-26 11:30:04 -0600

    full-app: bug testing

--- apps/full-app/app/page.tsx
+++ apps/full-app/app/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@daxwell/ui";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,6 +12,7 @@ export default function Home() {
           height={20}
           priority
         />
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Full app
