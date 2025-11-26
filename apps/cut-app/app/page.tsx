244d1cc7e6e39da7b999506779a4df2068376f07
dax-pavankumar-mistry <pavankumar.mistry@daxwell.com>
2025-11-26 11:17:00 -0600

    cut-app: update home page

--- apps/cut-app/app/page.tsx
+++ apps/cut-app/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Full app
+            Cut app
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -52,7 +52,7 @@ export default function Home() {
             Deploy Now
           </a>
           <Button>
-            Daxwell UI Button -- Full App
+            Daxwell UI Button -- Cut App
           </Button>
         </div>
       </main>
